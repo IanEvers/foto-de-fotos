c57c622ba6d00377b9ec823469e209e32a62eb9f
IanEvers <59263788+IanEvers@users.noreply.github.com>
2023-02-14 03:12:59 -0300

    aanshei

--- src/main.rs
+++ src/main.rs
@@ -4,231 +4,86 @@ extern crate rand;
 
 use std::fs::{self};
 use rand::Rng;
-use std::path::{Path, self};
-use std::thread;
-use std::time::{Instant};
-use image::imageops::{resize, FilterType};
-use image::{RgbImage, DynamicImage, ImageError, SubImage, open};
-use palette::{IntoColor, Lab,  Srgb, Pixel as OtherPixel};
-use palette::rgb::Rgb;
-use kmeans_colors::{get_kmeans, Kmeans, Sort};
-use deltae::*;
+use tqdm::Iter;
+use std::path::{Path};
+use std::time::{Instant, Duration};
+use image::imageops::{FilterType};
+use image::{RgbImage, ImageError, SubImage, DynamicImage};
 use rayon::prelude::*;
-
-struct ImagenLab {
-    ubicacion: String,
-    lab: Rgb
-}
+use std::sync::Mutex;
 
 fn main() {
-    // let paths = fs::read_dir("C:/Users/Ian/Desktop/imagenes/beautiful landscape/")
-    // .unwrap()
-    // .filter_map(|e| e.ok())
-    // .map(|e| e.path().to_string_lossy().into_owned())
-    // .collect::<Vec<_>>();
-
-    let imagen_objetivo = "C:/Users/Ian/Pictures/dalle.png";
-
-    // let dominante_imagenes = color_dominante_imagenes(paths);
+    
+    let imagen_objetivo = "C:/Users/Ian/Pictures/la_costa.jpg";
 
     let imagen_final_result = armar_imagen_objetivo(imagen_objetivo);
     
-    let mut rng = rand::thread_rng();
-
-    let random_value = rng.gen_range(0..100).to_string();
+    let random_value = rand::thread_rng().gen_range(0..100).to_string();
     
     let image_name = "imagenPrueba".to_owned() + &random_value + ".png";
+    println!("{}", image_name);
 
     match imagen_final_result {
         Ok(imagen) => imagen.save(image_name).unwrap(),
         Err(_) => println!("hubo error."),
     };
+
+    println!("listo");
 }
 
 fn load_images_from_dir(dir: &Path) -> Vec<RgbImage> {
 
-    // Obtenemos todos los archivos en el directorio
-    // let entries = fs::read_dir(dir).unwrap();
-   
     // Recorremos todos los archivos y cargamos las imágenes que encontremos
-
-    
     let paths = fs::read_dir(dir)
         .unwrap()
         .filter_map(|e| e.ok())
         .map(|e| e.path().to_string_lossy().into_owned())
         .collect::<Vec<_>>();
 
-    let images = paths.par_iter().map(|path| load_image(path)).collect();
+    let images = paths.par_iter().map(|path| image::open(path).unwrap().to_rgb8()).collect();
 
-
-    // for entry in entries {
-    //     let entry = entry.unwrap();
-    //     println!("{}", entry.path().to_string_lossy());
-
-        
-    //     // Verificamos si el archivo es una imagen
-    //     if let Some(ext) = entry.path().extension() {
-    //         if ext == "png" || ext == "jpg" || ext == "jpeg" || ext == "bmp" || ext == "gif" {
-    //             let start_abrir = Instant::now();
-    //             // Cargamos la imagen
-    //             let image: RgbImage = image::open(entry.path()).unwrap().to_rgb8();
-    //             let duracion_abrir = start_abrir.elapsed();
-    //             tiempo_abrir += duracion_abrir.as_millis();
-    //             println!("{}", tiempo_abrir);
-    //             images.push(image);
-    //         }
-    //     }
-    // }
     return images
-}
-
-fn load_image(image_path: &str) -> RgbImage {
-    println!("{}", image_path);
-    return image::open(image_path).unwrap().to_rgb8();
-}
-    
-fn color_dominante_imagenes(imagenes: Vec<String>) -> Vec<ImagenLab> {
-
-    println!("Arranco a analizar imágenes.");
-
-    let start = Instant::now();
-
-    let mut dominante_imagenes: Vec<ImagenLab> = Vec::new();
-
-    let mut tiempo_abrir_imagen: u128 = 0;  
-    let mut tiempo_analizar_imagen: u128 = 0; 
-
-    for (index, imagen) in imagenes.iter().enumerate() {
-        println!("{}/{} {}", index, imagenes.len(), imagenes[index]);
-        
-        let start_abrir = Instant::now();
-        let im: RgbImage = image::open(&imagen).unwrap().to_rgb8();
-        let duracion_abrir = start_abrir.elapsed();
-        tiempo_abrir_imagen += duracion_abrir.as_millis();
-
-        let start_analizar = Instant::now();
-
-        let im_width= im.width();
-        let im_height= im.height();
-
-        dominante_imagenes.push({
-            ImagenLab {
-                ubicacion: imagen.to_string(),
-                lab: color_promedio(&im, 0, im_width, 0, im_height)
-            }
-        });
-
-        let duracion_analizar = start_analizar.elapsed();
-        tiempo_analizar_imagen += duracion_analizar.as_millis();
-
-    }
-
-    println!("{} milisegundos en analizar", tiempo_analizar_imagen);
-    println!("{} milisegundos en abrir", tiempo_abrir_imagen);
-    
-    let duration = start.elapsed();
-
-    println!("Tardó {} segundos en analizar las imágenes recolectadas", duration.as_secs());
-
-    return dominante_imagenes;
-}
-
-fn color_dominante_imagen(imagen: RgbImage) -> Lab {
-    
-    let rgb: Vec<u8> = imagen.into_raw();
-
-    let lab: Vec<Lab> = Srgb::from_raw_slice(&rgb)
-    .iter()
-    .map(|x| x.into_format().into_color())
-    .collect();
-
-    // Iterate over the runs, keep the best results
-    let mut result = Kmeans::new();
-    for _i in 0..2 {
-        let run_result = get_kmeans(
-            2,
-            3,
-            0.9,
-            false,
-            &lab,
-            20000,
-        );
-        if run_result.score < result.score {
-            result = run_result;
-        }
-    }
-
-    // Using the results from the previous example, process the centroid data
-    let res = Lab::sort_indexed_colors(&result.centroids, &result.indices);
-
-    // We can find the dominant color directly
-    let dominant_color = Lab::get_dominant_color(&res);
-
-    // borrar unwrap
-    match dominant_color {
-        Some(color_lab) => return color_lab,
-        None => println!("Fallo obtener color dominante"),
-    };
-    //En caso de no encontrar color dominante se devuelve el 0
-    Lab::new(0.0, 0.0, 0.0)
-}
-
-fn color_promedio(imagen: &RgbImage, principio_x: u32, final_x: u32, principio_y: u32, final_y: u32) -> Rgb {
-    
-    let mut r_total: u32 = 0;
-    let mut g_total: u32 = 0;
-    let mut b_total: u32 = 0;
-
-    let area_seccion = (final_x - principio_x) * (final_y - principio_y);
-
-    for x in principio_x..final_x {
-        for y in principio_y..final_y {
-
-            let rgb = imagen.get_pixel(x, y).0;
-
-            r_total += rgb[0] as u32;
-            g_total += rgb[1] as u32;
-            b_total += rgb[2] as u32;
-        }
-    }
-
-    let r_promedio = (r_total / area_seccion) as f32;
-    let g_promedio = (g_total / area_seccion) as f32;
-    let b_promedio = (b_total / area_seccion) as f32;
-
-    let color_promedio_rgb: Rgb = Rgb::new(r_promedio.into() , g_promedio.into(), b_promedio.into());
-
-    // let color_promedio_lab: Lab = color_promedio_rgb.into_color();
-    
-    return color_promedio_rgb
-
 }
 
 fn armar_imagen_objetivo(imagen: &str) -> Result<RgbImage, ImageError> {
 
     println!("preparo.");
 
-    let start_preparo = Instant::now();
-    let filas : u32 = 100;
-    let columnas : u32 = 100;
+    let filas: u32 = 100;
+    let columnas: u32 = 100;
+
+    let ancho: u32 = 6400;
+    let alto: u32= 6400;
+
+    println!("ancho / columnas = {}, alto / filas = {}", ancho / columnas, alto / filas);
     
     let image = image::open(imagen).unwrap().to_rgb8();
-
-    let ancho = 4000;
-    let alto= 4000;
 
     let image_rgb_data = load_images_from_dir(Path::new("C:/Users/Ian/Desktop/imagenes/unsplash_resized"));
 
     let mut image_rgb_data_resized = Vec::new();
+
+    println!("empiezo a resizear todas las fotos");
+    
+    let start_resize: Instant = Instant::now();
 
     for image in image_rgb_data {
         let resized_image = image::imageops::resize(&image, ancho / columnas, alto / filas, FilterType::Nearest);
         image_rgb_data_resized.push(resized_image);
     }
 
-    let image_resized = image::imageops::resize(&image, ancho, alto, image::imageops::Nearest);
+    let duracion_resize: Duration = start_resize.elapsed();
 
+    println!("tardó {} segundos en resizear todas las fotos", duracion_resize.as_secs());
+
+    let start_img_resize: Instant = Instant::now();
+
+    let dynamic_image = DynamicImage::ImageRgb8(image);
+
+    let image_resized_dynamic: DynamicImage = DynamicImage::resize_exact(&dynamic_image, ancho, alto, image::imageops::Nearest);
+
+    let image_resized: RgbImage = image_resized_dynamic.into_rgb8();
+    
     let ancho_seccion: u32 = ancho / columnas;
     let alto_seccion: u32 = alto / filas;
 
@@ -237,17 +92,19 @@ fn armar_imagen_objetivo(imagen: &str) -> Result<RgbImage, ImageError> {
     let mut principio_seccion_x: u32;
     let mut principio_seccion_y: u32;
 
-    let duration = start_preparo.elapsed();
+    let duration = start_img_resize.elapsed();
 
-    println!("tarde {} en preparar.", duration.as_secs());
+    println!("tardé {} en resizear la imagen objetivo.", duration.as_secs());
 
     println!("empiezo.");
 
     let start2 = Instant::now();
 
-    for seccion in 0..(filas * columnas) {
+    let mut tiempo_subimagen: u128 = 0;
+    let mut tiempo_comparar_imagenes: u128 = 0;
+    let mut tiempo_overlay: u128 = 0;
 
-        println!("{} / {}", seccion, filas * columnas);
+    for seccion in (0..(filas * columnas)).into_iter().tqdm() {
 
         if seccion > 0 {
             principio_seccion_x = (seccion * ancho_seccion) % ancho;
@@ -257,88 +114,57 @@ fn armar_imagen_objetivo(imagen: &str) -> Result<RgbImage, ImageError> {
             principio_seccion_y = 0;
         }
 
-        let subimagen = SubImage::new(&image_resized, principio_seccion_x, principio_seccion_y, ancho_seccion, alto_seccion).to_image();
-        
-        // let subimagen_dynamic: DynamicImage = DynamicImage::ImageRgba8(subimagen);
+        let start_subimagen: Instant = Instant::now();
 
-        // let color_dominante_seccion = color_dominante_imagen(subimagen_dynamic.to_rgb8());
+        let subimagen: RgbImage = SubImage::new(&image_resized, principio_seccion_x, principio_seccion_y, ancho_seccion, alto_seccion).to_image();
         
+        tiempo_subimagen += start_subimagen.elapsed().as_millis();
 
-        // let color_promedio_seccion = color_promedio(&image, principio_seccion_x, principio_seccion_x + ancho_seccion, principio_seccion_y, principio_seccion_y + alto_seccion);
-
-        // let imagen_seccion = imagen_mas_cercana(color_promedio_seccion, &image_rgb_data);
-        
+        let start_comparar_imagenes: Instant = Instant::now();
+                
         let imagen_seccion: RgbImage = imagen_mas_cercana_exacto(&subimagen, &image_rgb_data_resized);
 
-        // let imagen_seccion_archivo: DynamicImage = image::open(&imagen_seccion.ubicacion).unwrap();
+        tiempo_comparar_imagenes += start_comparar_imagenes.elapsed().as_millis();
 
-        // let imagen_seccion_archivo_resized: RgbImage = imagen_seccion_archivo.resize_exact(ancho_seccion, alto_seccion, image::imageops::Nearest).to_rgb8();
+        let start_overlay = Instant::now();
         
         image::imageops::overlay(&mut img_final, &imagen_seccion, principio_seccion_x.into(), principio_seccion_y.into());
+        
+        tiempo_overlay += start_overlay.elapsed().as_millis();
+
     }
 
-    let duration2 = start2.elapsed();
+    let duration2: Duration = start2.elapsed();
 
     println!("terminó de cocinarse en {} segundos", duration2.as_secs());
-
+    println!("tomó {} milisegundos en hacer la subimagen", tiempo_subimagen);
+    println!("tomó {} milisegundos en hacer la comparacion", tiempo_comparar_imagenes);
+    println!("tomó {} milisegundos en hacer el overlay", tiempo_overlay);
+    
     return Ok(img_final)
 }
 
-fn imagen_mas_cercana(color_dominante_seccion: Rgb, dominante_imagenes: &Vec<ImagenLab>) -> ImagenLab {
-    let mut distancia_mas_cercana: f32 = 10000.0;
-    let mut imagen_mas_cerca: ImagenLab = ImagenLab { ubicacion: "null".to_string(), lab: Rgb::new(0.0, 0.0, 0.0) }; 
-    // let mut imagen_mas_cerca: ImagenLab = ImagenLab { ubicacion: "null".to_string(), lab: Lab { l: 1.0, a: 1.0, b: 1.0, white_point: PhantomData } }; 
-
-    for i in 0..dominante_imagenes.len() {
-        let distancia: f32 = distancia_entre_dos_colores_rgb(color_dominante_seccion, dominante_imagenes[i].lab);
-        if distancia_mas_cercana == 10000.0 || distancia < distancia_mas_cercana {
-            distancia_mas_cercana = distancia;
-            imagen_mas_cerca = ImagenLab {
-                ubicacion: dominante_imagenes[i].ubicacion.to_string(),
-                lab: dominante_imagenes[i].lab
-            };
-        }
-    }
-
-    return imagen_mas_cerca
-}
-
-fn distancia_entre_dos_colores_lab(lab1: Lab, lab2: Lab) -> f32 {
-    
-    let lab1 = LabValue {
-        l: lab1.l,
-        a: lab1.a,
-        b: lab1.b,
-    };
-
-    let lab2 = LabValue {
-        l: lab2.l,
-        a: lab2.a,
-        b: lab2.b,
-    };
-
-    return *DeltaE::new(&lab1, &lab2, DE2000).value();
-}
-
-fn distancia_entre_dos_colores_rgb(rgb1: Rgb, rgb2: Rgb) -> f32 {
-    return (rgb1.red - rgb2.red) * (rgb1.red - rgb2.red) + (rgb1.green - rgb2.green) * (rgb1.green - rgb2.green) + (rgb1.blue - rgb2.blue) * (rgb1.blue - rgb2.blue)
-}
-
-fn imagen_mas_cercana_exacto(imagen_objetivo: &RgbImage, images:& Vec<RgbImage>) -> RgbImage {
+fn imagen_mas_cercana_exacto(imagen_seccion: &RgbImage, images:& Vec<RgbImage>) -> RgbImage {
     // Función que encuentra la imagen que más se parece a una imagen de referencia 
-    let mut most_similar_image = image::ImageBuffer::new(0,0);
-    let mut most_similarity = 0.0;
 
-    for image in images {
-        let similarity = compare_images(&imagen_objetivo, &image);
+    // Use a Mutex to protect the most_similar_image variable
+    let most_similar_image: Mutex<image::ImageBuffer<image::Rgb<u8>, Vec<u8>>> = Mutex::new(image::ImageBuffer::new(0,0));
+    let most_similarity: Mutex<f64> = Mutex::new(0.0);
 
-        if similarity > most_similarity {
-            most_similar_image = image.clone();
-            most_similarity = similarity;
+        // Process each chunk in a separate thread
+    images.into_par_iter().for_each(|image| {
+        let similarity = compare_images(&imagen_seccion, &image);
+
+        // Update the most similar image if necessary
+        let mut most_similar_image = most_similar_image.lock().unwrap();
+        let mut most_similarity = most_similarity.lock().unwrap();
+        if similarity > *most_similarity {
+            *most_similar_image = image.clone();
+            *most_similarity = similarity;
         }
-    }
-
-    return most_similar_image
+    });
+    
+    return (*most_similar_image.lock().unwrap()).clone();
 }
 
 // Función que compara dos imágenes pixel por pixel y devuelve su parecido en términos de porcentaje
@@ -352,29 +178,43 @@ fn compare_images(image1: &RgbImage, image2: &RgbImage) -> f64 {
     for (x, y, pixel1) in image1.enumerate_pixels() {
         let pixel2 = image2.get_pixel(x, y);
 
-        if color_close_enough(pixel1, pixel2, 25.0) {
+        if color_close_enough(pixel1, pixel2, 20.0) {
             num_pixels_equal += 1;
         }
     }
 
     // Calculamos el porcentaje de parecido entre las imágenes
     let num_pixels = (image1.width() * image1.height()) as f64;
-    let similarity = (num_pixels_equal as f64 / num_pixels) * 50.0;
+    let similarity = num_pixels_equal as f64 / num_pixels;
 
     return similarity
 }
 
 fn color_close_enough(rgb1: &image::Rgb<u8>, rgb2: &image::Rgb<u8>, closeness: f64) -> bool {
   
-    let red_diff = (rgb1.0[0] as i32 - rgb2.0[0] as i32).abs();
-    let green_diff = (rgb1.0[1] as i32 - rgb2.0[1] as i32).abs();
-    let blue_diff = (rgb1.0[2] as i32 - rgb2.0[2] as i32).abs();
+    let red_diff = (rgb1.0[0] as f32 - rgb2.0[0] as f32).abs() * 0.299;
+    let green_diff = (rgb1.0[1] as f32 - rgb2.0[1] as f32).abs() * 0.587;
+    let blue_diff = (rgb1.0[2] as f32 - rgb2.0[2] as f32).abs() * 0.114;
 
     let distance = (red_diff + green_diff + blue_diff) as f64;
 
-    if distance < closeness {
-        return true;
-    } else {
-        return false;
-    }
+    return distance < closeness 
 }
+
+// fn color_close_enough_gpu(rgb1: &GpuMat<Vec3b>, rgb2: &GpuMat<Vec3b>, closeness: f64) -> GpuMat<u8> {
+//     let mut result = GpuMat::default();
+// 
+//     // Create a scalar value with the desired closeness threshold
+//     let threshold = Scalar::new(closeness, 0.0, 0.0, 0.0);
+// 
+//     // Calculate the absolute difference between the two images
+//     cuda::absdiff(rgb1, rgb2, &mut result);
+// 
+//     // Sum the values of all channels of the difference image
+//     cuda::sum(result, &mut result);
+// 
+//     // Threshold the result to get a binary image indicating which pixels are close enough
+//     cuda::threshold(&result, &mut result, threshold, 1.0, cv::THRESH_BINARY);
+// 
+//     return result;
+// }
